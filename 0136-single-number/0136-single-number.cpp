adff077ffcc94220b26d3aea0be902517feea123
mmalaika.junaid <mmalaika.junaid@gmail.com>
2024-03-11 22:21:46 +0400

    Time: 15 ms (56.92%), Space: 19.2 MB (77.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,8 @@
-class Solution 
-{
+class Solution {
 public:
-    int singleNumber(vector<int>& nums) 
-    {
+    int singleNumber(vector<int>& nums) {
         int s_num = 0;
         for (int n : nums)
-        {
             s_num ^= n;
-        }
-        return s_num;
-    }
+        return s_num;}
 };
